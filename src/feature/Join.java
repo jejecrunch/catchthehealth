f03916614bce0a6f0406cd5062298e30a3d14765
jejecrunch <gkswlgp456@gmail.com>
2018-11-16 03:04:11 +0900

    04 join 회원가입 창 폼 view 구현

--- src/feature/Join.java
+++ src/feature/Join.java
@@ -1,28 +1,199 @@
 package feature;
 
-import javax.swing.JPanel;
-import java.awt.Color;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.SwingConstants;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.GroupLayout.Alignment;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
 
 public class Join extends JPanel {
+	private JTextField idF;
+	private JTextField pwF;
+	private JTextField pwcF;
+	private JTextField nameF;
+	private JTextField emailF;
+	private JTextField phonenumberF;
+	private JTextField phonenumber2F;
+	private JTextField addressF;
 
-	/**
-	 * Create the panel.
-	 */
 	public Join() {
 		setForeground(new Color(255, 255, 255));
 		setBackground(new Color(204, 204, 255));
 		setBounds(100,100,800,600);
 		setLayout(null);
 		
-		JLabel label = new JLabel("가입하기");
-		label.setBounds(14, 12, 150, 50);
-		label.setHorizontalAlignment(SwingConstants.CENTER);
-		label.setForeground(new Color(255, 255, 255));
-		label.setFont(new Font("맑은 고딕", Font.BOLD, 22));
-		add(label);
+		// top
+		JLabel joinus = new JLabel("가입하기");
+		joinus.setBounds(14, 12, 150, 50);
+		joinus.setHorizontalAlignment(SwingConstants.CENTER);
+		joinus.setForeground(new Color(255, 255, 255));
+		joinus.setFont(new Font("맑은 고딕", Font.BOLD, 22));
+		add(joinus);
+		
+		JButton okB = new JButton("OK");
+		okB.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		okB.setForeground(Color.WHITE);
+		okB.setFont(new Font("Calibri", Font.PLAIN, 15));
+		okB.setBackground(new Color(204, 204, 255));
+		okB.setBounds(330, 500, 100, 25);
+		okB.setBorderPainted(false);
+		okB.setFocusPainted(false);
+		okB.setContentAreaFilled(false);
+		add(okB);
 
+		// 
+		JPanel p=new JPanel();
+		
+		p.setOpaque(false);
+		
+		p.setBounds(200, 115, 350, 330);
+		
+		add(p);
+						p.setLayout(null);
+						
+						JLabel idL = new JLabel("ID");
+						idL.setBounds(0, 0, 125, 25);
+						idL.setHorizontalAlignment(SwingConstants.RIGHT);
+						idL.setBackground(new Color(204, 204, 255));
+						idL.setFont(new Font("Calibri", Font.BOLD, 15));
+						idL.setForeground(Color.WHITE);
+						p.add(idL);
+						
+						idF = new JTextField();
+						idF.setBounds(150, 0, 125, 25);
+						idF.setFont(new Font("Calibri", Font.PLAIN, 15));
+						idF.setColumns(10);
+						p.add(idF);
+						
+						JLabel pwL = new JLabel("PASSWORD");
+						pwL.setBounds(0, 35, 125, 25);
+						pwL.setHorizontalAlignment(SwingConstants.RIGHT);
+						pwL.setForeground(Color.WHITE);
+						pwL.setFont(new Font("Calibri", Font.BOLD, 15));
+						pwL.setBackground(new Color(204, 204, 255));
+						p.add(pwL);
+						
+						pwF = new JPasswordField();
+						pwF.setBounds(150, 35, 126, 25);
+						pwF.setFont(new Font("Calibri", Font.PLAIN, 15));
+						pwF.setColumns(10);
+						p.add(pwF);
+						
+						JLabel pwcL = new JLabel("PASSWORD");
+						pwcL.setBounds(0, 70, 125, 25);
+						pwcL.setHorizontalAlignment(SwingConstants.RIGHT);
+						pwcL.setForeground(Color.WHITE);
+						pwcL.setFont(new Font("Calibri", Font.BOLD, 15));
+						pwcL.setBackground(new Color(204, 204, 255));
+						p.add(pwcL);
+						
+						pwcF = new JPasswordField();
+						pwcF.setBounds(150, 70, 125, 25);
+						pwcF.setFont(new Font("Calibri", Font.PLAIN, 15));
+						pwcF.setColumns(10);
+						p.add(pwcF);
+						
+						JButton checkB = new JButton("CHECK");
+						checkB.setBounds(280, 70, 80, 25);
+						checkB.setBackground(new Color(204, 204, 255));
+						checkB.setFont(new Font("Calibri", Font.BOLD, 15));
+						checkB.setForeground(new Color(255, 255, 255));
+						checkB.setBorderPainted(false);
+						checkB.setFocusPainted(false);
+						checkB.setContentAreaFilled(false);
+						p.add(checkB);
+						
+						JLabel nameL = new JLabel("NAME");
+						nameL.setBounds(0, 105, 125, 25);
+						nameL.setHorizontalAlignment(SwingConstants.RIGHT);
+						nameL.setForeground(Color.WHITE);
+						nameL.setFont(new Font("Calibri", Font.BOLD, 15));
+						nameL.setBackground(new Color(204, 204, 255));
+						p.add(nameL);
+						
+						nameF = new JTextField();
+						nameF.setBounds(150, 105, 125, 25);
+						nameF.setFont(new Font("Calibri", Font.PLAIN, 15));
+						nameF.setColumns(10);
+						p.add(nameF);
+						
+						JLabel ageL = new JLabel("AGE");
+						ageL.setBounds(0, 140, 125, 25);
+						ageL.setHorizontalAlignment(SwingConstants.RIGHT);
+						ageL.setForeground(Color.WHITE);
+						ageL.setFont(new Font("Calibri", Font.BOLD, 15));
+						ageL.setBackground(new Color(204, 204, 255));
+						p.add(ageL);
+						
+								JComboBox ageRange = new JComboBox();
+								ageRange.setBounds(150, 140, 125, 25);
+								ageRange.setForeground(new Color(255, 255, 255));
+								ageRange.setBackground(new Color(204, 204, 255));
+								ageRange.setFont(new Font("맑은 고딕", Font.PLAIN, 15));
+								ageRange.setModel(new DefaultComboBoxModel(new String[] {"10대", "20대", "30대", "40대", "50대", "60대", "70대", "80대 이상"}));
+								p.add(ageRange);
+						
+						JLabel phonenumberL = new JLabel("PHONE NUMBER");
+						phonenumberL.setBounds(0, 175, 125, 25);
+						phonenumberL.setHorizontalAlignment(SwingConstants.RIGHT);
+						phonenumberL.setForeground(Color.WHITE);
+						phonenumberL.setFont(new Font("Calibri", Font.BOLD, 15));
+						phonenumberL.setBackground(new Color(204, 204, 255));
+						p.add(phonenumberL);
+						
+						phonenumberF = new JTextField();
+						phonenumberF.setBounds(150, 175, 125, 25);
+						phonenumberF.setFont(new Font("Calibri", Font.PLAIN, 15));
+						phonenumberF.setColumns(10);
+						p.add(phonenumberF);
+						
+								JLabel phonenumber2L = new JLabel("PHONE NUMBER2");
+								phonenumber2L.setBounds(0, 210, 125, 25);
+								phonenumber2L.setHorizontalAlignment(SwingConstants.RIGHT);
+								phonenumber2L.setForeground(Color.WHITE);
+								phonenumber2L.setFont(new Font("Calibri", Font.BOLD, 15));
+								phonenumber2L.setBackground(new Color(204, 204, 255));
+								p.add(phonenumber2L);
+						
+						phonenumber2F = new JTextField();
+						phonenumber2F.setBounds(150, 210, 125, 25);
+						phonenumber2F.setFont(new Font("Calibri", Font.PLAIN, 15));
+						phonenumber2F.setColumns(10);
+						p.add(phonenumber2F);
+						
+						JLabel emailL = new JLabel("EMAIL");
+						emailL.setBounds(0, 245, 125, 25);
+						emailL.setHorizontalAlignment(SwingConstants.RIGHT);
+						emailL.setForeground(Color.WHITE);
+						emailL.setFont(new Font("Calibri", Font.BOLD, 15));
+						emailL.setBackground(new Color(204, 204, 255));
+						p.add(emailL);
+						
+						emailF = new JTextField();
+						emailF.setBounds(150, 245, 125, 25);
+						emailF.setFont(new Font("Calibri", Font.PLAIN, 15));
+						emailF.setColumns(10);
+						p.add(emailF);
+				
+				JLabel addressL = new JLabel("ADDRESS");
+				addressL.setBounds(0, 280, 125, 25);
+				addressL.setHorizontalAlignment(SwingConstants.RIGHT);
+				addressL.setForeground(Color.WHITE);
+				addressL.setFont(new Font("Calibri", Font.BOLD, 15));
+				addressL.setBackground(new Color(204, 204, 255));
+				p.add(addressL);
+		
+		addressF = new JTextField();
+		addressF.setBounds(150, 280, 125, 50);
+		addressF.setFont(new Font("Calibri", Font.PLAIN, 15));
+		addressF.setColumns(10);
+		p.add(addressF);
 	}
 }
